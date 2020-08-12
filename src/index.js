8b0a852b0eabb4edcfdece862626fe7408c23bb2
acarolren <anacarolinareisdesa@gmail.com>
2020-08-12 16:12:51 -0300

    who is the winner

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice()
+    if (calculaWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -37,7 +40,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    const winner = calculaWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else{
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
 
     return (
       <div>
@@ -79,6 +88,25 @@ class Game extends React.Component {
   }
 }
 
+function calculaWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 7],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]
+    }
+  }
+  return null
+}
 // ========================================
 
 ReactDOM.render(
